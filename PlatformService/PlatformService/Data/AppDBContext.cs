49a1ffdec6dbb78716cec8bf24d6c6e40ef88032
Nathan Soares Oliveira <50963930+attkniat@users.noreply.github.com>
2023-07-01 14:24:33 -0300

    AppDBContext class creation, and setting to startup class

---
+++ PlatformService/PlatformService/Data/AppDBContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PlatformService.Model;
+
+namespace PlatformService.Data
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> opt) : base(opt) { }
+
+        public DbSet<Platform> Platforms { get; set; }
+    }
+}
