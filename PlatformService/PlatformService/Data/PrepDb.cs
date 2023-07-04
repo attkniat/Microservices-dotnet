40114fbea3796757668fa9c7e129061976e41a1a
Nathan Soares Oliveira <50963930+attkniat@users.noreply.github.com>
2023-07-04 18:19:39 -0300

    Update PrepData to use in the runing env

--- PlatformService/PlatformService/Data/PrepDb.cs
+++ PlatformService/PlatformService/Data/PrepDb.cs
@@ -1,28 +1,42 @@
-﻿using PlatformService.Model;
+﻿using Microsoft.EntityFrameworkCore;
+using PlatformService.Model;
 
 namespace PlatformService.Data
 {
     public static class PrepDb
     {
-        public static void PrepPolulation(IApplicationBuilder app)
+        public static void PrepPolulation(IApplicationBuilder app, bool isprod)
         {
             using (var serviceScope = app.ApplicationServices.CreateScope())
             {
-                SeedData(serviceScope.ServiceProvider.GetService<AppDBContext>());
+                SeedData(serviceScope.ServiceProvider.GetService<AppDBContext>(), isprod);
             }
         }
 
-        private static void SeedData(AppDBContext context)
+        private static void SeedData(AppDBContext context, bool isprod)
         {
+            if (isprod)
+            {
+                Console.WriteLine("--> Attempting to apply migrations...");
+                try
+                {
+                    context.Database.Migrate();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Could not run migrations: {ex.Message}");
+                }
+            }
+
             if (!context.Platforms.Any())
             {
                 Console.WriteLine("--> Seeding Data...");
 
                 context.Platforms.AddRange(
-                    new Platform() {Name = "DOTNET", Publlisher="Microsoft", Cost="free"},
-                    new Platform() {Name = "SQL Server Express", Publlisher="Microsoft", Cost="free"},    
+                    new Platform() { Name = "DOTNET", Publlisher = "Microsoft", Cost = "free" },
+                    new Platform() { Name = "SQL Server Express", Publlisher = "Microsoft", Cost = "free" },
                     new Platform() { Name = "Kubernets", Publlisher = "Cloud Native Computing Foundation", Cost = "free" }
-                ); 
+                );
                 context.SaveChanges();
             }
             else
