9c7d4cd62fdec6a55873a91491b55e7d6426e9b4
Nathan Soares Oliveira <50963930+attkniat@users.noreply.github.com>
2023-07-06 14:44:52 -0300

    AppDbContext adding + DI Command

--- PlatformService/CommandsService/CommandsService/Program.cs
+++ PlatformService/CommandsService/CommandsService/Program.cs
@@ -1,6 +1,11 @@
+using CommandsService.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMen"));
+builder.Services.AddScoped<ICommandRepo, CommandRepo>();
 
 var app = builder.Build();
 //app.UseHttpsRedirection();
