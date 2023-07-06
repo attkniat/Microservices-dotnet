808d32bf0c8a6913f84ca60909fa40e2686dec2c
Nathan Soares Oliveira <50963930+attkniat@users.noreply.github.com>
2023-07-06 14:37:19 -0300

    Dto's mappings

--- PlatformService/CommandsService/CommandsService/Program.cs
+++ PlatformService/CommandsService/CommandsService/Program.cs
@@ -1,17 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
 builder.Services.AddControllers();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-
 //app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
