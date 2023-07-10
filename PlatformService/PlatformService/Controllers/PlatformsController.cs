92a9e5a6dafbeaae73ed098b9fefd1f9536025e4
Nathan Soares Oliveira <50963930+attkniat@users.noreply.github.com>
2023-07-10 02:29:29 +0000

    Send Async Message to RabbiotMQ messageBus client

--- PlatformService/PlatformService/Controllers/PlatformsController.cs
+++ PlatformService/PlatformService/Controllers/PlatformsController.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using PlatformService.AsyncDataServices;
 using PlatformService.Data;
 using PlatformService.Dtos;
 using PlatformService.Model;
@@ -15,11 +16,14 @@ namespace PlatformService.Controllers
         private readonly IPlatformRepo _repository;
         private readonly IMapper _mapper;
 
-        public PlatformsController(IPlatformRepo repository, IMapper mapper, ICommandDataClient commandDataClient)
+        private readonly IMessageBusClient _messageBusClient;
+
+        public PlatformsController(IPlatformRepo repository, IMapper mapper, ICommandDataClient commandDataClient, IMessageBusClient messageBusClient)
         {
             _repository = repository;
             _mapper = mapper;
             this._commandDataClient = commandDataClient;
+            _messageBusClient = messageBusClient;
         }
 
         [HttpGet]
@@ -63,17 +67,17 @@ namespace PlatformService.Controllers
                 Console.WriteLine($"--> Could not send synchronously: {ex.Message}");
             }
 
-            ////Send Async Message
-            //try
-            //{
-            //    var platformPublishedDto = _mapper.Map<PlatformPublishedDto>(platformReadDto);
-            //    platformPublishedDto.Event = "Platform_Published";
-            //    _messageBusClient.PublishNewPlatform(platformPublishedDto);
-            //}
-            //catch (Exception ex)
-            //{
-            //    Console.WriteLine($"--> Could not send asynchronously: {ex.Message}");
-            //}
+            //Send Async Message
+            try
+            {
+               var platformPublishedDto = _mapper.Map<PlatformPublishedDto>(platformReadDto);
+               platformPublishedDto.Event = "Platform_Published";
+               _messageBusClient.PublishNewPlatform(platformPublishedDto);
+            }
+            catch (Exception ex)
+            {
+               Console.WriteLine($"--> Could not send asynchronously: {ex.Message}");
+            }
 
             return CreatedAtRoute(nameof(GetPlatformById), new { Id = platformReadDto.Id }, platformReadDto);
         }
