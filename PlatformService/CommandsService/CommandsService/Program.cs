a984bc33d31f7f0dd5ce14c7f285b6e8a468f245
Nathan Soares Oliveira <50963930+attkniat@users.noreply.github.com>
2023-07-10 02:58:11 +0000

    Setting DI to EventProcessor

--- PlatformService/CommandsService/CommandsService/Program.cs
+++ PlatformService/CommandsService/CommandsService/Program.cs
@@ -1,4 +1,5 @@
 using CommandsService.Data;
+using CommandsService.EventProcessing;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,6 +7,7 @@ builder.Services.AddControllers();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMen"));
 builder.Services.AddScoped<ICommandRepo, CommandRepo>();
+builder.Services.AddSingleton<IEventProcessor, EventProcessor>();
 
 var app = builder.Build();
 //app.UseHttpsRedirection();
