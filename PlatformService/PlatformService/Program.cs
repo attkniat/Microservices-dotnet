57e81f1592e9a9f4c311a63b8c569e634c866c23
Nathan Soares Oliveira <50963930+attkniat@users.noreply.github.com>
2023-07-12 05:17:22 +0000

    Added the gRPC server service file, Mapping to GrpcPlatformModel + Mapping the gRPC services to our controllers + Mapping a Get Route to write the proto file.

--- PlatformService/PlatformService/Program.cs
+++ PlatformService/PlatformService/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using PlatformService.AsyncDataServices;
 using PlatformService.Data;
+using PlatformService.SyncDataServices.Grpc;
 using PlatformService.SyncDataServices.Http;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,8 +10,7 @@ var env = builder.Environment;
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
+//builder.Services.AddSwaggerGen();
 
 if (env.IsProduction())
 {
@@ -27,20 +27,28 @@ builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddHttpClient<ICommandDataClient, HttpCommandDataClient>();
 builder.Services.AddSingleton<IMessageBusClient, MessageBusClient>();
+builder.Services.AddGrpc();
 
 Console.WriteLine($"---> CommandService Endpoint {settings["CommandService"]}");
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
 
 //app.UseHttpsRedirection();
 app.UseAuthorization();
+
 app.MapControllers();
+app.MapGrpcService<GrpcPlatformService>();
+
+app.MapGet("/protos/platforms.proto", async context =>
+{
+    await context.Response.WriteAsync(File.ReadAllText("/protos/platforms.proto"));
+});
 
 PrepDb.PrepPolulation(app, env.IsProduction());
 
