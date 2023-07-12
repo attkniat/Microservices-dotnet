9febdfcd72c5add06c1b48ad659b1d088c3a1531
Nathan Soares Oliveira <50963930+attkniat@users.noreply.github.com>
2023-07-12 07:50:29 +0000

    Db Preparation to consume the Grpc service

--- PlatformService/CommandsService/CommandsService/Program.cs
+++ PlatformService/CommandsService/CommandsService/Program.cs
@@ -1,6 +1,7 @@
 using CommandsService.AsyncDataServices;
 using CommandsService.Data;
 using CommandsService.EventProcessing;
+using CommandsService.SyncDataServices.Grpc;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,9 +12,13 @@ builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMen"));
 builder.Services.AddScoped<ICommandRepo, CommandRepo>();
 builder.Services.AddSingleton<IEventProcessor, EventProcessor>();
+builder.Services.AddScoped<IPlatformDataClient, PlatformDataClient>();
 
 var app = builder.Build();
-//app.UseHttpsRedirection();
+app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
+
+PrepDb.PrepPopulation(app);
+
 app.Run();
